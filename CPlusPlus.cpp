9512b8f1817316a13362c76b66a3e8c179600245
JanKoch99 <jankoch99@bluewin.ch>
2023-06-30 10:40:42 +0200

    Version1

--- CPlusPlus.cpp
+++ CPlusPlus.cpp
@@ -20,8 +20,8 @@ int main() {
 
     #ifdef n==3
         t1 = 0;
-    #endif teststring = "Base"
-        t1 = 2;
+    #endif teststring = "Version1"
+        t1 = 1;
     #ifdef
 
     for (int i = 1; i <= n; ++i) {
