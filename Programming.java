1ad16c84142af99ed104e320f0499062d66d0e66
JanKoch99 <jankoch99@bluewin.ch>
2023-11-26 14:00:43 +0100

    BaseCommit

---
+++ Programming.java
@@ -0,0 +1,11 @@
+public class Programming {
+    Integer variableOne = 0;
+
+    void incrementVariableOne() {
+        this.variableOne ++;
+    }
+
+    void clearVariable() {
+        this.variableOne = 0;
+    }
+}
